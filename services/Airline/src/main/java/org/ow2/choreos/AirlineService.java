1bb9d22b2a35306f6bfdc7d53a598f211b26263f
Leonardo Leite <leonardofl87@gmail.com>
2013-12-14 12:29:49 -0200

    Airline service now uses log4j

--- services/Airline/src/main/java/org/ow2/choreos/AirlineService.java
+++ services/Airline/src/main/java/org/ow2/choreos/AirlineService.java
@@ -2,16 +2,19 @@ package org.ow2.choreos;
 
 import javax.jws.WebService;
 
-import org.ow2.choreos.log.SimpleLogger;
-import org.ow2.choreos.log.SimpleLoggerImpl;
+import org.apache.log4j.Logger;
 
 @WebService(targetNamespace = "http://choreos.ow2.org/", endpointInterface = "org.ow2.choreos.Airline")
 public class AirlineService implements Airline {
 
     public static final String FLIGHT_NUMBER = "33";
 
-    private final SimpleLogger logger = new SimpleLoggerImpl("/tmp/airline.log");
+    private static Logger logger = Logger.getLogger(AirlineService.class);
 
+    static {
+        LogConfigurator.configLog();
+    }
+    
     public AirlineService() {
         logger.info("Airline started at " + AirlineStarter.SERVICE_ADDRESS);
     }
