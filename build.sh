701ff21a9238708fcd0578a2981818561d3f274f
Leonardo Leite <leonardofl87@gmail.com>
2014-12-09 23:59:18 -0200

    Deleting repository, it will be no more necessary

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-# Attention: it may be necessary to copy the content of the repository folder to your local maven repository
-# cp repository/org/ow2/ ~/.m2/repository/org/ow2/
-# cp repository/com/ebmwebsourcing/ ~/.m2/repository/com/ebmwebsourcing/
 mvn clean javadoc:jar source:jar install && mvn eclipse:clean eclipse:eclipse
